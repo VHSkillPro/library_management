d116bfb3e72dd1b826ebd73fb5002d1532d481b0
Hazzu <hailaconsutu558@gmail.com>
2023-12-15 21:20:47 +0700

    add layout panel book

--- src/view/PanelBook.java
+++ src/view/PanelBook.java
@@ -2,10 +2,17 @@ package view;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.Rectangle;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class PanelBook extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTable tableListBook;
 
 	/**
 	 * Create the panel.
@@ -15,10 +22,39 @@ public class PanelBook extends JPanel {
 		createContents();
 	}
 	private void createContents() {
+		setBounds(new Rectangle(0, 0, 1035, 680));
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Đây là panel của sách");
-		lblNewLabel.setBounds(153, 138, 128, 13);
-		add(lblNewLabel);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 62, 1015, 608);
+		add(scrollPane);
+		
+		tableListBook = new JTable();
+		scrollPane.setColumnHeaderView(tableListBook);
+		
+		JLabel labelTitle = new JLabel("Quản lý sách trong thư viện");
+		labelTitle.setFont(new Font("Segoe UI", Font.BOLD, 25));
+		labelTitle.setBounds(10, 11, 400, 40);
+		add(labelTitle);
+		
+		JButton buttonAdd = new JButton("Thêm");
+		buttonAdd.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		buttonAdd.setBounds(595, 20, 100, 30);
+		add(buttonAdd);
+		
+		JButton buttonFind = new JButton("Tìm kiếm");
+		buttonFind.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		buttonFind.setBounds(705, 20, 100, 30);
+		add(buttonFind);
+		
+		JButton btnDelete = new JButton("Xoá");
+		btnDelete.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		btnDelete.setBounds(925, 20, 100, 30);
+		add(btnDelete);
+		
+		JButton btnEdit = new JButton("Chỉnh sửa");
+		btnEdit.setFont(new Font("Segoe UI", Font.PLAIN, 12));
+		btnEdit.setBounds(815, 20, 100, 30);
+		add(btnEdit);
 	}
 }
