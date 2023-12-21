cb99e9ad04e2fe58f5a758e826849cde2d167568
Hazzu <hailaconsutu558@gmail.com>
2023-12-21 23:08:47 +0700

    re-design layout FormEditDocGia.java

--- src/view/FormEditDocGia.java
+++ src/view/FormEditDocGia.java
@@ -23,31 +23,33 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.util.Date;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
 public class FormEditDocGia extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private DocGia docGia;
+	
 	private JPanel contentPane;
-
-	private PanelDocGia parent;
 	private JTextField inputHoTen;
 	private JTextField inputEmail;
 	private JTextField inputSoDienThoai;
 	private JTextField inputDiaChi;
 	private JComboBox<String> comboGioiTinh;
 	private JDateChooser dateChooserNgaySinh;
-	
-	private boolean enableEditor = false;
 	private JButton buttonEditEnable;
 	private JButton buttonExist;
 	
+	private DocGia docGia;
+	private boolean enableEditor = false;
+	
+	private PanelDocGia parent;
 	private FormEditDocGia thisForm = this;
 	
 	/**
 	 * Create the frame.
 	 */
 	public FormEditDocGia(int maDocGia, PanelDocGia parent) {
+		setResizable(false);
 		this.parent = parent;
 		this.docGia = DocGiaBo.getDocGiaByMaDocGia(maDocGia);
 		createContents();
@@ -115,7 +117,7 @@ public class FormEditDocGia extends JFrame {
 	private void createContents() {
 		setTitle("Thông tin độc giả - Phần mềm quản lý thư viện");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 500, 410);
+		setBounds(100, 100, 400, 500);
 		contentPane = new JPanel();
 		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setBackground(new Color(240, 240, 240));
@@ -126,112 +128,119 @@ public class FormEditDocGia extends JFrame {
 		
 		JLabel labelTitle = new JLabel("Thông tin độc giả");
 		labelTitle.setFont(new Font("Segoe UI", Font.BOLD, 20));
-		labelTitle.setBounds(150, 15, 180, 30);
+		labelTitle.setBounds(110, 15, 180, 30);
 		contentPane.add(labelTitle);
 		
 		JLabel labelMaDocGia = new JLabel("Mã độc giả:");
-		labelMaDocGia.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelMaDocGia.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelMaDocGia.setBounds(20, 70, 90, 25);
+		labelMaDocGia.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/user.png")));
+		labelMaDocGia.setHorizontalAlignment(SwingConstants.LEFT);
+		labelMaDocGia.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelMaDocGia.setBounds(20, 80, 100, 16);
 		contentPane.add(labelMaDocGia);
 		
 		JLabel labelMaDocGiaValue = new JLabel(String.valueOf(docGia.getMaDocGia()));
 		labelMaDocGiaValue.setHorizontalAlignment(SwingConstants.LEFT);
-		labelMaDocGiaValue.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		labelMaDocGiaValue.setBounds(120, 70, 90, 25);
+		labelMaDocGiaValue.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		labelMaDocGiaValue.setBounds(120, 80, 250, 16);
 		contentPane.add(labelMaDocGiaValue);
 		
-		JLabel labelFullname = new JLabel("Họ và tên:");
-		labelFullname.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelFullname.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelFullname.setBounds(20, 110, 90, 25);
+		JLabel labelFullname = new JLabel("Họ và tên");
+		labelFullname.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/id-card.png")));
+		labelFullname.setHorizontalAlignment(SwingConstants.LEFT);
+		labelFullname.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelFullname.setBounds(20, 110, 100, 16);
 		contentPane.add(labelFullname);
 		
 		inputHoTen = new JTextField();
 		inputHoTen.setEditable(false);
 		inputHoTen.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		inputHoTen.setBounds(120, 110, 340, 25);
+		inputHoTen.setBounds(20, 130, 350, 25);
 		inputHoTen.setText(docGia.getHoTen());
 		contentPane.add(inputHoTen);
 		inputHoTen.setColumns(10);
 		
-		JLabel labelGender = new JLabel("Giới tính:");
-		labelGender.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelGender.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelGender.setBounds(294, 150, 80, 25);
+		JLabel labelGender = new JLabel("Giới tính");
+		labelGender.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/gender.png")));
+		labelGender.setHorizontalAlignment(SwingConstants.LEFT);
+		labelGender.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelGender.setBounds(290, 170, 80, 16);
 		contentPane.add(labelGender);
 		
 		comboGioiTinh = new JComboBox<String>();
 		comboGioiTinh.setEnabled(false);
 		comboGioiTinh.setModel(new DefaultComboBoxModel<String>(new String[] {"Nam", "Nữ"}));
 		comboGioiTinh.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		comboGioiTinh.setBounds(384, 150, 76, 25);
+		comboGioiTinh.setBounds(290, 190, 80, 25);
 		comboGioiTinh.setSelectedIndex(docGia.getGioiTinh() ? 0 : 1);
 		contentPane.add(comboGioiTinh);
 		
-		JLabel labelBirthdate = new JLabel("Ngày sinh:");
-		labelBirthdate.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelBirthdate.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelBirthdate.setBounds(20, 150, 90, 25);
+		JLabel labelBirthdate = new JLabel("Ngày sinh");
+		labelBirthdate.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/date-of-birth.png")));
+		labelBirthdate.setHorizontalAlignment(SwingConstants.LEFT);
+		labelBirthdate.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelBirthdate.setBounds(20, 170, 100, 16);
 		contentPane.add(labelBirthdate);
 		
 		dateChooserNgaySinh = new JDateChooser(docGia.getNgaySinh(), "dd/MM/yyyy");
 		dateChooserNgaySinh.setEnabled(false);
 		dateChooserNgaySinh.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		dateChooserNgaySinh.setBounds(120, 150, 180, 25);
+		dateChooserNgaySinh.setBounds(20, 190, 260, 25);
 		contentPane.add(dateChooserNgaySinh);
 		
-		JLabel labelEmail = new JLabel("Email:");
-		labelEmail.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelEmail.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelEmail.setBounds(20, 190, 90, 25);
+		JLabel labelEmail = new JLabel("Email");
+		labelEmail.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/mail.png")));
+		labelEmail.setHorizontalAlignment(SwingConstants.LEFT);
+		labelEmail.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelEmail.setBounds(20, 290, 100, 16);
 		contentPane.add(labelEmail);
 		
 		inputEmail = new JTextField();
 		inputEmail.setEditable(false);
 		inputEmail.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		inputEmail.setColumns(10);
-		inputEmail.setBounds(120, 190, 340, 25);
+		inputEmail.setBounds(20, 310, 350, 25);
 		inputEmail.setText(docGia.getEmail());
 		contentPane.add(inputEmail);
 		
-		JLabel labelSoDienThoai = new JLabel("Số điện thoại:");
-		labelSoDienThoai.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelSoDienThoai.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelSoDienThoai.setBounds(10, 230, 100, 25);
+		JLabel labelSoDienThoai = new JLabel("Số điện thoại");
+		labelSoDienThoai.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/phone-call.png")));
+		labelSoDienThoai.setHorizontalAlignment(SwingConstants.LEFT);
+		labelSoDienThoai.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelSoDienThoai.setBounds(20, 230, 100, 16);
 		contentPane.add(labelSoDienThoai);
 		
 		inputSoDienThoai = new JTextField();
 		inputSoDienThoai.setEditable(false);
 		inputSoDienThoai.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		inputSoDienThoai.setColumns(10);
-		inputSoDienThoai.setBounds(120, 230, 340, 25);
+		inputSoDienThoai.setBounds(20, 250, 350, 25);
 		inputSoDienThoai.setText(docGia.getSoDienThoai());
 		contentPane.add(inputSoDienThoai);
 		
-		JLabel labelDiaChi = new JLabel("Địa chỉ:");
-		labelDiaChi.setHorizontalAlignment(SwingConstants.RIGHT);
-		labelDiaChi.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		labelDiaChi.setBounds(20, 270, 90, 25);
+		JLabel labelDiaChi = new JLabel("Địa chỉ");
+		labelDiaChi.setIcon(new ImageIcon(FormEditDocGia.class.getResource("/icons/pin.png")));
+		labelDiaChi.setHorizontalAlignment(SwingConstants.LEFT);
+		labelDiaChi.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		labelDiaChi.setBounds(20, 350, 100, 16);
 		contentPane.add(labelDiaChi);
 		
 		inputDiaChi = new JTextField();
 		inputDiaChi.setEditable(false);
 		inputDiaChi.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		inputDiaChi.setColumns(10);
-		inputDiaChi.setBounds(120, 270, 340, 25);
+		inputDiaChi.setBounds(20, 370, 350, 25);
 		inputDiaChi.setText(docGia.getDiaChi());
 		contentPane.add(inputDiaChi);
 		
 		JSeparator separator = new JSeparator();
 		separator.setForeground(new Color(160, 160, 160));
 		separator.setBackground(new Color(255, 255, 255));
-		separator.setBounds(20, 61, 440, 2);
+		separator.setBounds(20, 60, 350, 2);
 		contentPane.add(separator);
 		
 		buttonEditEnable = new JButton("Chỉnh sửa");
 		buttonEditEnable.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		buttonEditEnable.setBounds(240, 320, 120, 30);
+		buttonEditEnable.setBounds(190, 410, 120, 30);
 		contentPane.add(buttonEditEnable);
 		
 		buttonExist = new JButton("Quay lại");
@@ -241,7 +250,7 @@ public class FormEditDocGia extends JFrame {
 			}
 		});
 		buttonExist.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		buttonExist.setBounds(130, 320, 100, 30);
+		buttonExist.setBounds(80, 410, 100, 30);
 		contentPane.add(buttonExist);
 	}
 }
