e8ad978d41be8d0cfd17307535351d29562e4e83
Hazzu <hailaconsutu558@gmail.com>
2023-12-21 23:31:51 +0700

    add validate FormEditDocGia.java

--- src/view/FormEditDocGia.java
+++ src/view/FormEditDocGia.java
@@ -6,6 +6,9 @@ import javax.swing.border.EmptyBorder;
 
 import bean.DocGia;
 import bo.DocGiaBo;
+import utils.Validate;
+import utils.ValidateElement;
+import utils.ValidateForm;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -18,6 +21,7 @@ import javax.swing.DefaultComboBoxModel;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JSeparator;
 import java.awt.Color;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
@@ -44,6 +48,11 @@ public class FormEditDocGia extends JFrame {
 	
 	private PanelDocGia parent;
 	private FormEditDocGia thisForm = this;
+
+	private JLabel labelErrorFullname;
+	private JLabel labelErrorPhone;
+	private JLabel labelErrorEmail;
+	private JLabel labelErrorAddress;
 	
 	/**
 	 * Create the frame.
@@ -71,44 +80,85 @@ public class FormEditDocGia extends JFrame {
 					buttonEditEnable.setText("Lưu");
 				}
 				else {
-					int decision = JOptionPane.showConfirmDialog(null, "Bạn có muốn lưu ?");
-			
-					if (decision == 0) {
-						String hoTen = inputHoTen.getText();
-						boolean gioiTinh = comboGioiTinh.getSelectedIndex() == 0 ? true : false;
-						Date ngaySinh = dateChooserNgaySinh.getDate();
-						String email = inputEmail.getText();
-						String soDienThoai = inputSoDienThoai.getText();
-						String diaChi = inputDiaChi.getText();
+					boolean haveError = false;
+					labelErrorFullname.setText("");
+					labelErrorPhone.setText("");
+					labelErrorEmail.setText("");
+					labelErrorAddress.setText("");
+					
+					String hoTen = inputHoTen.getText();
+					boolean gioiTinh = comboGioiTinh.getSelectedIndex() == 0 ? true : false;
+					Date ngaySinh = dateChooserNgaySinh.getDate();
+					String soDienThoai = inputSoDienThoai.getText();
+					String email = inputEmail.getText();
+					String diaChi = inputDiaChi.getText();
+					
+					ValidateForm<String> validateFullname = new ValidateForm<String>();
+					validateFullname.addElement(new ValidateElement<String>("Vui lòng nhập trường này", str -> Validate.isExist(str)));
+					String messageErrorFullname = validateFullname.validate(hoTen);
+					if (messageErrorFullname != null) {
+						haveError = true;
+						labelErrorFullname.setText(messageErrorFullname);
+					}
+					
+					ValidateForm<String> validatePhone = new ValidateForm<String>();
+					validatePhone.addElement(new ValidateElement<String>("Vui lòng nhập trường này", str -> Validate.isExist(str)));
+					validatePhone.addElement(new ValidateElement<String>("Số điện thoại không hợp lệ", str -> Validate.isNumberPhone(str)));
+					String messageErrorPhone = validatePhone.validate(soDienThoai);
+					if (messageErrorPhone != null) {
+						haveError = true;
+						labelErrorPhone.setText(messageErrorPhone);
+					}
+					
+					ValidateForm<String> validateEmail = new ValidateForm<String>();
+					validateEmail.addElement(new ValidateElement<String>("Vui lòng nhập trường này", str -> Validate.isExist(str)));
+					validateEmail.addElement(new ValidateElement<String>("Email không hợp lệ", str -> Validate.isEmail(str)));
+					String messageErrorEmail = validateEmail.validate(email);
+					if (messageErrorEmail != null) {
+						haveError = true;
+						labelErrorEmail.setText(messageErrorEmail);
+					}
+					
+					ValidateForm<String> validateAddress = new ValidateForm<String>();
+					validateAddress.addElement(new ValidateElement<String>("Vui lòng nhập trường này", str -> Validate.isExist(str)));
+					String messageErrorAddress = validateAddress.validate(diaChi);
+					if (messageErrorAddress != null) {
+						haveError = true;
+						labelErrorAddress.setText(messageErrorAddress);
+					}
+					
+					if (!haveError) {
+						int decision = JOptionPane.showConfirmDialog(null, "Bạn có muốn lưu ?");
+						if (decision == 0) {
+							if (DocGiaBo.updateDocGia(docGia.getMaDocGia(), hoTen, gioiTinh, ngaySinh, email, soDienThoai, diaChi)) {
+								JOptionPane.showMessageDialog(null, "Lưu thành công");
+								parent.loadTable(DocGiaBo.getAllDocGia());
+							}
+							else {
+								JOptionPane.showMessageDialog(null, "Lưu thất bại", "Lỗi", JOptionPane.ERROR_MESSAGE);
+							}
+						}
 						
-						if (DocGiaBo.updateDocGia(docGia.getMaDocGia(), hoTen, gioiTinh, ngaySinh, email, soDienThoai, diaChi)) {
-							JOptionPane.showMessageDialog(null, "Lưu thành công");
-							parent.loadTable(DocGiaBo.getAllDocGia());
+						if (decision != 2) {
+							enableEditor = false;
+							inputHoTen.setEditable(false);
+							comboGioiTinh.setEnabled(false);
+							dateChooserNgaySinh.setEnabled(false);
+							inputEmail.setEditable(false);
+							inputSoDienThoai.setEditable(false);
+							inputDiaChi.setEditable(false);
+							buttonExist.setEnabled(true);
+							buttonEditEnable.setText("Chỉnh sửa");
 						}
-						else {
-							JOptionPane.showMessageDialog(null, "Lưu thất bại", "Lỗi", JOptionPane.ERROR_MESSAGE);
-						}
+						
+						docGia = DocGiaBo.getDocGiaByMaDocGia(docGia.getMaDocGia());
+						inputHoTen.setText(docGia.getHoTen());
+						comboGioiTinh.setSelectedIndex(docGia.getGioiTinh() ? 0 : 1);
+						dateChooserNgaySinh.setDate(docGia.getNgaySinh());
+						inputEmail.setText(docGia.getEmail());
+						inputSoDienThoai.setText(docGia.getSoDienThoai());
+						inputDiaChi.setText(docGia.getDiaChi());
 					}
-					
-					if (decision != 2) {
-						enableEditor = false;
-						inputHoTen.setEditable(false);
-						comboGioiTinh.setEnabled(false);
-						dateChooserNgaySinh.setEnabled(false);
-						inputEmail.setEditable(false);
-						inputSoDienThoai.setEditable(false);
-						inputDiaChi.setEditable(false);
-						buttonExist.setEnabled(true);
-						buttonEditEnable.setText("Chỉnh sửa");
-					}
-					
-					docGia = DocGiaBo.getDocGiaByMaDocGia(docGia.getMaDocGia());
-					inputHoTen.setText(docGia.getHoTen());
-					comboGioiTinh.setSelectedIndex(docGia.getGioiTinh() ? 0 : 1);
-					dateChooserNgaySinh.setDate(docGia.getNgaySinh());
-					inputEmail.setText(docGia.getEmail());
-					inputSoDienThoai.setText(docGia.getSoDienThoai());
-					inputDiaChi.setText(docGia.getDiaChi());
 				}
 			}
 		});
@@ -252,5 +302,33 @@ public class FormEditDocGia extends JFrame {
 		buttonExist.setFont(new Font("Segoe UI", Font.BOLD, 14));
 		buttonExist.setBounds(80, 410, 100, 30);
 		contentPane.add(buttonExist);
+		
+		labelErrorFullname = new JLabel("");
+		labelErrorFullname.setForeground(Color.RED);
+		labelErrorFullname.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelErrorFullname.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		labelErrorFullname.setBounds(120, 110, 250, 16);
+		contentPane.add(labelErrorFullname);
+		
+		labelErrorPhone = new JLabel("");
+		labelErrorPhone.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelErrorPhone.setForeground(Color.RED);
+		labelErrorPhone.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		labelErrorPhone.setBounds(120, 230, 250, 16);
+		contentPane.add(labelErrorPhone);
+		
+		labelErrorEmail = new JLabel("");
+		labelErrorEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelErrorEmail.setForeground(Color.RED);
+		labelErrorEmail.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		labelErrorEmail.setBounds(120, 290, 250, 16);
+		contentPane.add(labelErrorEmail);
+		
+		labelErrorAddress = new JLabel("");
+		labelErrorAddress.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelErrorAddress.setForeground(Color.RED);
+		labelErrorAddress.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		labelErrorAddress.setBounds(120, 350, 250, 16);
+		contentPane.add(labelErrorAddress);
 	}
 }
